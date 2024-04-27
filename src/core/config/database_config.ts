cd1af065895271467c1640705ba11d6f10f8f95c
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2024-04-27 17:06:43 +0300

    update database_config data

--- src/core/config/database_config.ts
+++ src/core/config/database_config.ts
@@ -3,12 +3,25 @@ import { Todo } from 'src/entity/todo.entity';
 import { User } from 'src/entity/user.entity';
 
 export const dbConfig: TypeOrmModuleOptions = {
-  database: 'Todo_db',
   type: 'postgres',
-  port: 5433,
-  username: 'postgres',
-  password: '123456789',
+  // port: 5433,
+  // database: 'Todo_db',
+  // username: 'postgres',
+  // password: '123456789',
+  port: 5432,
+  database: 'todo_db_giua',
+  username: 'postgres_user',
+  password: 'yjdOq5mpCsdgzfFYHBIPjbHyht0rCD5x',
   host: '127.0.0.1',
   //   entities: ['dist/**/*.entity{.ts,.js}'],
   entities: [Todo, User],
 };
+
+/*
+Internal Database URL:-  
+postgres://postgres_user:yjdOq5mpCsdgzfFYHBIPjbHyht0rCD5x@dpg-comfhmq1hbls73f2n3ng-a/todo_db_giua
+
+External Database URL:-
+postgres://postgres_user:yjdOq5mpCsdgzfFYHBIPjbHyht0rCD5x@dpg-comfhmq1hbls73f2n3ng-a.oregon-postgres.render.com/todo_db_giua
+
+*/
